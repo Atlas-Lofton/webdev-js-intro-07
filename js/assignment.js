90571978758687f378c64b148fcbfc4f9c77c9d1
Atlas-Lofton <gll4392@Hotmail.com>
2025-01-18 10:08:13 -0600

    updated code

--- js/assignment.js
+++ js/assignment.js
@@ -6,3 +6,48 @@ const checkIfEvenElement = document.getElementById("check-if-even");
 const lostNumbersElement = document.getElementById("lost-numbers");
 
 const lostNumbers = [4, 8, 15, 16, 23, 42];
+
+function returnRemainder() {
+    let x = 5 % 4;
+    remainderElement.innerText = x;
+}
+
+function checkIfEven() {
+    let answer;
+    if (47 % 2 == 0) {
+        answer = true;
+    }
+    else {answer = false;}
+    checkIfEvenElement.innerText = answer;
+} 
+
+
+function getTheFourthElement() {
+    let fourth = lostNumbers[3];
+    lostNumbersElement.innerText = fourth;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function render() {
+  returnRemainder();  
+   checkIfEven(); 
+   getTheFourthElement();
+}
+
+submissionBtn.addEventListener("click", function () {
+    render();
+});
+
+
